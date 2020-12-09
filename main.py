29ffcc2812ec218763e8d0fc6445de7160f81c3b
Pawel Sleziona <pawel.sleziona@gmail.com>
2020-12-09 19:43:01 +0100

    moving through walls, little refactor

--- main.py
+++ main.py
@@ -15,32 +15,37 @@ class Board:
         self.food_counter = 0
         self.food = []
         while True:
-
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            # Reset screen and count fps
             self.screen.fill((0, 0, 0))
             self.time_count += self.frame.tick() / 300
             if self.time_count > 0.5:
                 self.snake.move()
                 self.time_count -= 0.5
 
+            # Keep snake on surface
             self.snake.draw()
+
+            # Food generator
             if self.food_counter == 0:
-                self.food_gen()
+                self.food_generator()
             if len(self.food):
                 pygame.draw.rect(self.screen, (200, 40, 80), self.food[0])
-            self.check_is_eat()
+            self.check_snake_eat()
+
             pygame.display.flip()
 
-    def food_gen(self):
+    def food_generator(self):
         self.food_counter += 1
-        foodx, foody = Vector2(random.randrange(0, 1280, 20), random.randrange(0, 640, 20))
+        foodx, foody = Vector2(random.randrange(0, self.screen.get_width(), self.snake.size),
+                               random.randrange(0, self.screen.get_height(), self.snake.size))
         self.food.append((foodx, foody, 20, 20))
 
-    def check_is_eat(self):
-        if len(self.food) > 0:
+    def check_snake_eat(self):
+        if len(self.food):
             fx, fy = self.food[0][0], self.food[0][1]
             if fx == self.snake.snake[-1].x and fy == self.snake.snake[-1].y:
                 self.snake.eat()
@@ -53,13 +58,15 @@ class Snake:
         self.board = board
         # Starting points
         self.start_point = Vector2(x, y)
-        self.snake = [pygame.Rect(self.start_point.x - 40, self.start_point.y, 20, 20),
-                      pygame.Rect(self.start_point.x - 20, self.start_point.y, 20, 20),
-                      pygame.Rect(self.start_point, (20, 20))]
+        self.size = 20
+        self.snake = [pygame.Rect(self.start_point.x - 2 * self.size, self.start_point.y, self.size, self.size),
+                      pygame.Rect(self.start_point.x - self.size, self.start_point.y, self.size, self.size),
+                      pygame.Rect(self.start_point, (self.size, self.size))]
         self.move_direction = 'right'
 
     def draw(self):
         for block in self.snake:
+            block.x, block.y = self.thru_wall(block.x, block.y)
             pygame.draw.rect(self.board.screen, (200, 0, 100), block)
 
     def move(self):
@@ -85,29 +92,44 @@ class Snake:
         cx, cy = self.snake[-1].x, self.snake[-1].y
         self.snake.pop(0)
         if dir == 'down':
-            cy += 20
+            cy += self.size
             self.move_direction = 'down'
         elif dir == 'up':
-            cy -= 20
+            cy -= self.size
             self.move_direction = 'top'
-        elif dir == 'right':  # Dojebac szerokosc wensza jako zmienna
-            cx -= 20
+        elif dir == 'right':
+            cx -= self.size
             self.move_direction = 'left'
         elif dir == 'left':
-            cx += 20
+            cx += self.size
             self.move_direction = 'right'
-        if cx == 1280:
-            self.snake[-1].x = 0
-        if cy == 720:
-            self.snake[-1].y = 0
-        print(cx, cy)
-        self.snake.append(pygame.Rect(cx, cy, 20, 20))
+        self.snake.append(pygame.Rect(cx, cy, self.size, self.size))
 
     def eat(self):
         tailx, taily = self.snake[0].x, self.snake[0].y
-        self.snake[0:0] = [pygame.Rect(tailx, taily, 20, 20)]
+        self.snake[0:0] = [pygame.Rect(tailx, taily, self.size, self.size)]
         self.board.food = []
         self.board.food_counter -= 1
 
+    def check_self_coll(self):
+        # Sprawdzic czy powtarzaja sie x, y
+        for elem in self.snake:
+            pass
+
+    def thru_wall(self, x, y):
+        max_w = self.board.screen.get_width()
+        max_h = self.board.screen.get_height()
+        if x >= max_w:
+            x -= max_w
+        if y >= max_h:
+            y -= max_h
+        if x < 0 and self.move_direction == 'left':
+            x += self.board.screen.get_width()
+        if y < 0 and self.move_direction == 'top':
+            y += self.board.screen.get_height()
+        return x, y
+
 
 Board()
+
+
